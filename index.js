74fd7c8ca4aa257df62469dd745e9db2316a794a
patilvedanti42 <your_email_for_patilvedanti42@gmail.com>
2025-11-29 23:23:38 +0530

    Remove app.listen for Vercel

--- index.js
+++ index.js
@@ -19,7 +19,4 @@ app.use("/files", fileRoutes);
 app.use("/ai", aiRoutes);
 
 // Start Server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
